3c5daac94fec368c34b3d0cb680b93038c84e8b8
Tom Hodder <tom@limepepper.co.uk>
2025-08-02 12:30:44 +0100

    checkpoint add some doc images, add icon for plugin

--- src/main/kotlin/org/limepepper/gdb_plugin/GdbFileType.kt
+++ src/main/kotlin/org/limepepper/gdb_plugin/GdbFileType.kt
@@ -1,5 +1,6 @@
 package org.limepepper.gdb_plugin
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
@@ -14,5 +15,5 @@ object GdbFileType : LanguageFileType(GdbLanguage.INSTANCE) {
     
     override fun getDefaultExtension() = "gdb"
     
-    override fun getIcon(): Icon? = null // TODO: Add custom icon in future phases
+    override fun getIcon(): Icon? = AllIcons.Actions.StartDebugger
 }
